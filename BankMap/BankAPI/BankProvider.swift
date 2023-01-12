2127f15f4445c3d657fd9005d004697bd532afff
SaYan4ik <66200306+SaYan4ik@users.noreply.github.com>
2023-01-12 21:49:17 +0300

    Update provider

--- BankMap/BankAPI/BankProvider.swift
+++ BankMap/BankAPI/BankProvider.swift
@@ -20,7 +20,6 @@ final class GetBankInfo {
                     guard let bank = try? response.mapArray(BankModel.self) else { return }
                     complition(bank)
                 case .failure(let error):
-                    print(error.localizedDescription)
                     failure?()
             }
         }
